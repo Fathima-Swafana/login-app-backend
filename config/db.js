26253e5d3c9dec104dce4e4a4544e0ba7c1c1831
Fathima-Swafana <fathimaswafana@gmail.com>
2026-06-20 07:21:13 +0530

    Update database config for Render

--- config/db.js
+++ config/db.js
@@ -1,20 +1,11 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-console.log("===== DB CONFIG =====");
-console.log("HOST:", process.env.DB_HOST);
-console.log("PORT:", process.env.DB_PORT);
-console.log("USER:", process.env.DB_USER);
-console.log("PASSWORD:", process.env.DB_PASSWORD);
-console.log("DATABASE:", process.env.DB_NAME);
-console.log("=====================");
-
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
